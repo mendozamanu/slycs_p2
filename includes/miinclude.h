396e0c9ca9c284a7be5850836806df64bb7fa043
Manuel Mendoza Hurtado <i52mehum@uco.es>
2019-03-04 18:49:52 +0100

    RamaA merge

--- includes/miinclude.h
+++ includes/miinclude.h
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <string.h>
+
 void f()
 {
-	printf("Hello World \n");
+	char c1[100]="Hola mundo";
+	char c2[100]=", soy Manuel Mendoza. \n";
+	printf ("%s\n", strcat(c1, c2));
+	return;
 }
